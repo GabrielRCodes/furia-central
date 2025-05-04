0d3be814039ba315404488e585aad5a1ce2c5c1d
GabrielRCodes <gabrielrcodes@gmail.com>
2025-05-04 10:39:16 -0300

    :hammer: fix(Lint): fixing lint errors

--- src/app/community/components/ContactFormModal.tsx
+++ src/app/community/components/ContactFormModal.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { z } from 'zod';
-import { User } from '@prisma/client';
 import {
   Dialog,
   DialogContent,
